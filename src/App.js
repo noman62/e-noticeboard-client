c61fb8f1a5d4ab447a5e5aa831b2b66491a95700
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-04-26 15:09:50 +0600

    1st

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home/Home";
 import Homepage from "./components/Homepage/Homepage";
 import LatestNotice from "./components/LatestNotice/LatestNotice";
 import UpcomingNotice from "./components/UpcomingNotice/UpcomingNotice";
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Home />} />
       <Route path="dashboard/latestNews" element={<LatestNotice />} />
       <Route path="dashboard/upcomingNews" element={<UpcomingNotice />} />
     </Routes>
