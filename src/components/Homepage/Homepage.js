55ff5fd995af504a2694908e42be5e1bbb40e75f
noman62 <noman.ice.nstu@gmail.com>
2022-06-03 22:59:13 -0700

    change

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -89,7 +89,7 @@ const Homepage = () => {
 
           </div>
           <div class="col-md-8 p-1">
-            <div id="demo" class="carousel slide" data-ride="carousel">
+            <div id="demo" class="carousel slide" data-ride="carousel" data-interval="10000" >
 
               <ul class="carousel-indicators">
                 <li data-target="#demo" data-slide-to="0" class="active"></li>
@@ -170,7 +170,7 @@ const Homepage = () => {
           </div>
           <div class="col-2 top-time ">
             <p class="mb-0">
-              {/* <CurrentDate /> */}
+              <CurrentDate />
             </p>
 
           </div>
